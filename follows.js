ecc3ec103189147bccbd10ac84b598ae3badd01f
bonestm <bonestm@gmail.com>
2014-05-20 14:50:20 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,11 +14,11 @@
 
 	var defaultKey		= 'y3olOoEkJ4XcD70w9uaWvoSQ0GDWPRzvXFF4qJcxyhPU2TK3', // Unique master Xively API key to be used as a default
 		defaultFeeds	= '78092935', // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+		applicationName	= 'Petes Freezer Monitor', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
